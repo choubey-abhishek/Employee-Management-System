ff440abe5e4057bfc6d7061f5781c6bde19fe73f
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-02-06 11:15:06 +0530

    Create AttendanceService.java

---
+++ src/main/java/com/ems/service/AttendanceService.java
@@ -0,0 +1,33 @@
+package com.ems.service;
+
+import com.ems.entity.Attendance;
+import com.ems.entity.Employee;
+import com.ems.repository.AttendanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class AttendanceService {
+
+    @Autowired
+    private AttendanceRepository attendanceRepository;
+
+    public List<Attendance> getAllAttendance() {
+        return attendanceRepository.findAll();
+    }
+
+    public List<Attendance> getAttendanceByEmployee(Employee employee) {
+        return attendanceRepository.findByEmployee(employee);
+    }
+
+    public Attendance saveAttendance(Attendance attendance) {
+        return attendanceRepository.save(attendance);
+    }
+
+    public void deleteAttendance(Long id) {
+        attendanceRepository.deleteById(id);
+    }
+}
