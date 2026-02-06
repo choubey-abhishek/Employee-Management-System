7bb9a82d4998997c7585d9b1329c46c2ba167e16
Abhishek choubey <Abhishekchoubey312@gmail.com>
2026-02-06 11:14:11 +0530

    Create CustomUserDetailsService.java

---
+++ src/main/java/com/ems/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.ems.service;
+
+import com.ems.entity.Employee;
+import com.ems.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Employee employee = employeeRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        GrantedAuthority authority = new SimpleGrantedAuthority(employee.getRole());
+        Collection<GrantedAuthority> authorities = Collections.singletonList(authority);
+
+        return new User(employee.getUsername(), employee.getPassword(), authorities);
+    }
+}
